4331a1ab79e4a1e3838f0c3384cb65ecd805de3b
Stefan van Oudenaarden <s.oudenaarden@gmail.com>
2025-07-07 17:43:54 +0200

    Adding a proper readme

--- Sufficient Velocity/SV Readermode/sv-readingview.user.js
+++ Sufficient Velocity/SV Readermode/sv-readingview.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
-// @name         SV Readermode
+// @name         SV Reading View
 // @namespace    http://tampermonkey.net/
 // @version      2025-07-07
 // @description  Add reading mode buttons to individual posts on SV forums. This reader mode uses a solarized light colour scheme and respects SV colours and glows. It reveals invisitext and makes clear it was invisible with a slight change in colour and a dotted underline.
 // @author       Stefan van Oudenaarden
 // @match        https://forums.sufficientvelocity.com/threads/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=sufficientvelocity.com
-// @downloadURL  https://github.com/Stefan-v-Oudenaarden/Userscripts/raw/refs/heads/main/Sufficient%20Velocity/SV%20Readermode/sv-readermode.user.js
+// @downloadURL
 // @grant        GM_setValue
 // @grant        GM_getValue
 // ==/UserScript==
