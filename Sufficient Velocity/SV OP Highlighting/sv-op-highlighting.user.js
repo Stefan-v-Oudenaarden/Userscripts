1a18736c05bc2e8508a52eec07d2381d2fba8f28
Stefan van Oudenaarden <s.oudenaarden@gmail.com>
2026-03-12 08:43:20 +0100

    feat: SB support for op highlighting

--- Sufficient Velocity/SV OP Highlighting/sv-op-highlighting.user.js
+++ Sufficient Velocity/SV OP Highlighting/sv-op-highlighting.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         SV OP Highlighting
 // @namespace    http://tampermonkey.net/
-// @version      2025-05-28
+// @version      2026-12-03
 // @description  try to take over the world!
 // @author       Stefan van Oudenaarden
-// @match        https://forums.sufficientvelocity.com/threads/*
+// @include      https://forums.sufficientvelocity.com/threads/*
+// @include      https://forums.spacebattles.com/threads/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=sufficientvelocity.com
 // @downloadURL  https://github.com/Stefan-v-Oudenaarden/Userscripts/raw/refs/heads/main/Sufficient%20Velocity/SV%20OP%20Highlighting/sv-op-highlighting.user.js
 // @grant        none
@@ -17,7 +18,14 @@
   if (OPName && posts) {
     for (let post of posts) {
       if (post.innerText === OPName) {
-        post.parentElement.parentElement.parentElement.setAttribute("style", "background: #1B2938;");
+        if (window.location.href.includes("sufficientvelocity")) {
+          post.parentElement.parentElement.parentElement.setAttribute("style", "background: #1B2938;");
+        }
+
+        if (window.location.href.includes("spacebattles")) {
+          console.log(post.parentElement.parentElement.parentElement);
+          post.parentElement.parentElement.parentElement.setAttribute("style", "background: #496898;");
+        }
       }
     }
   }
